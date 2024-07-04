00570e367edb19e856baf57c075959a6eef2b39e
vigneshwaran1409 <mvigneshwaran1409@gmail.com>
2024-07-04 15:30:18 +0530

    nodetask3feupdate changes

--- student/src/App.jsx
+++ student/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AppRoutes from './components/utils/AppRoutes'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-export const API_URL =''
+export const API_URL ='https://nodejstask3be.onrender.com/'
 function App() {
   const router = createBrowserRouter(AppRoutes)
   return <>
